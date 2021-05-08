dd1cfdba318d00cb3b3e2b286731524844c755e4
Thomas Opdebeeck <thomas.opdebeeck@realdolmen.com>
2021-05-09 00:40:10 +0200

    Set up CI/CD deployment Step 1: create simple util & test

---
+++ hybrid/utils/__tests/helloWorld.test.js
@@ -0,0 +1,16 @@
+import {getHelloValue} from "../helloWord.util";
+
+describe('Hello world util', () => {
+    describe('WHEN I pass nothing', () => {
+        test('THEN I expect a simple Hello as retur', () => {
+            const result = getHelloValue();
+            expect(result).toBe('Hello');
+        });
+    });
+    describe('WHEN I pass a valid string value', () => {
+        test('THEN I expect to be greeted correctly', () => {
+            const result = getHelloValue('Lord Voldemort');
+            expect(result).toBe('Hello, Lord Voldemort');
+        });
+    });
+});
