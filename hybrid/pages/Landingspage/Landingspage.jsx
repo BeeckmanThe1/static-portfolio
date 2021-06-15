dfbe85864176aab402a0ad1c94b35336c17bcd6b
Thomas Opdebeeck <thomas.opdebeeck@realdolmen.com>
2021-06-15 23:51:33 +0200

    Add redux store.js
    Clean up: improve code

--- hybrid/pages/Landingspage/Landingspage.jsx
+++ hybrid/pages/Landingspage/Landingspage.jsx
@@ -5,7 +5,7 @@ const LandingsPage = () => {
     return <>
         <h1>Index</h1>
         <ul>
-            {WEBSITE_SETUP.ALL_PAGES.map(PAGE => PAGE?.SLUG !== '/' && <li><a href={PAGE?.SLUG}>{PAGE?.TITLE}</a></li>)}
+            {WEBSITE_SETUP.ALL_PAGES.map(PAGE => PAGE?.SLUG !== '/' && <li key={PAGE.TITLE}><a href={PAGE?.SLUG}>{PAGE?.TITLE}</a></li>)}
         </ul>
     </>;
 };
